d93e8c47526e1a15fcb7598c4d549f50f59fd5cf
yashita7002@gmail.com <yashita7002@gmail.com>
2026-03-25 12:26:41 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ require ('dotenv').config({path: './env'})*/
 
 import dotenv from "dotenv"
 import connectDB from"./db/index.js"
-import {app} from "/app.js"
+import {app} from "./app.js"
 
 dotenv.config({
     path:'./.env'
